2629140031040d752e9c35490a0f8ba4b94f9670
Iselin Olguin <you@example.com>
2020-10-16 00:42:16 -0500

    Agregando

---
+++ Eje.py
@@ -0,0 +1,14 @@
+def song_decoder(song):
+    song="AWUBBWUBC"
+    song = song.replace('WUB',' ')
+    print(song)
+    return song
+    song ="AWUBWUBWUBBWUBWUBWUBC"
+    song = song.replace('WUB','  ')
+    print(song)
+    return song
+    song ="WUBAWUBBWUBCWUB"
+    song = song.replace('WUB',' ')
+    print(song)
+    return song
+song_decoder()
